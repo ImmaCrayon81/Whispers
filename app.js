2b555620b6c68745d652abc1ab74f14019dafd5a
ImmACrayon <enyangpaeey@gmail.com>
2022-02-19 20:59:54 +0800

    add bcrypt

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ const express = require("express");
 const ejs = require("ejs");
 const app = express();
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
 mongoose.connect("mongodb://localhost:27017/userDB");
 
 app.set('view engine', 'ejs');
-app.use(express.urlencoded({extended: true})); //bodyParser replacement
+app.use(express.urlencoded({
+  extended: true
+})); //bodyParser replacement
 app.use(express.static("public"));
 
 
@@ -17,8 +21,6 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password']});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -33,36 +35,42 @@ app.get("/login", function(req, res) {
 app.post("/login", function(req, res) {
   const username = req.body.username;
   const password = req.body.password;
-  User.findOne({email: username}, function(err, foundUser) {
+  User.findOne({
+    email: username
+  }, function(err, foundUser) {
     if (err) {
       console.log(err);
     } else {
       if (foundUser) {
-        if (foundUser.password === password) {
-          res.render("secrets");
-        }
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if (result === true) {
+            res.render("secrets");
+          }
+        });
       }
     }
-  })
-})
+  });
+});
 
 app.get("/register", function(req, res) {
   res.render("register");
 });
 
 app.post("/register", function(req, res) {
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save(function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
   });
-  newUser.save(function(err) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("secrets");
-    }
-  })
-})
+});
 
 
 app.listen(3000, function() {
